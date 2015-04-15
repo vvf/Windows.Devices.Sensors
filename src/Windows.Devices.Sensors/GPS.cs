9807bfa135b0c8da2a6ceeab4e757b5e242e6195
Vladimir Fedorenko <vvf7723@gmail.com>
2015-04-15 09:05:35 +0300

    add GPSInternal.cs and GPS.cs

---
+++ src/Windows.Devices.Sensors/GPS.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Sensors.Metadata;
+using Windows.Devices.Sensors.ObjectModel;
+
+namespace Windows.Devices.Sensors
+{
+    public class GPS
+    {
+        public static GPS GetDefault()
+        {
+            try
+            {
+                var sensorList = SensorManager.GetSensorsByTypeId<GPSInternal>();
+                if (sensorList.Count > 0)
+                {
+                    return new GPS(sensorList[0]);
+                }
+            }
+            catch
+            {
+            }
+            return null;
+        }
+
+        private Sensors.GPSInternal m_gps;
+
+		private GPS(Sensors.GPSInternal gps)
+		{
+			m_gps = gps;
+			m_gps.DataReportChanged += DataReportChanged;
+            m_gps.StateChanged += onStateChanged;
+		}
+        private void onStateChanged(Sensor sender, EventArgs e)
+        {
+            if (StateChanged != null)
+            {
+                StateChanged(this, EventArgs.Empty);
+            }
+        }
+		private void DataReportChanged(Sensor sender, EventArgs e)
+		{
+			if (ReadingChanged != null)
+			{
+                var location = m_gps.CurrentLocation;
+				ReadingChanged(this, new GPSReadingChangedEventArgs(
+                    new GPSReading(location)));
+			}
+		}
+		
+        public event EventHandler<GPSReadingChangedEventArgs> ReadingChanged;
+        public event EventHandler<EventArgs> StateChanged;
+
+		public GPSReading GetCurrentReading()
+		{
+			var location = m_gps.CurrentLocation;
+            return new GPSReading(location);
+		}
+
+		public uint MinimumReportInterval 
+		{
+			get { return m_gps.MinimumReportInterval; }
+		}
+
+		public uint ReportInterval
+		{
+			get { return m_gps.ReportInterval; }
+			set { m_gps.ReportInterval = value; }
+		}
+        public String State
+        {
+            get { return m_gps.State.ToString(); }
+        }
+	}
+
+    public sealed class GPSReadingChangedEventArgs : EventArgs
+	{
+		internal GPSReadingChangedEventArgs(GPSReading reading)
+		{
+			Reading = reading;
+		}
+		public GPSReading Reading { get; private set; }
+	}
+
+	public sealed class GPSReading
+	{
+        internal GPSReading(Location _location)
+		{
+            location = _location;
+		}
+        public Location location { get; private set; }
+	}
+    public class Location
+    {
+        public double Latitude;
+        public double Longitude;
+        public double Accuracy;
+        public DateTimeOffset Timestamp;
+        public String NMEA;
+        public Object nativeReportValues;
+        public string debugString;
+        public double Speed;
+        public int Satelites;
+        public override String ToString()
+        {
+#if DEBUG
+            return debugString;
+#else
+            return "Location (" + Latitude + ", " + Longitude + ") ~" + Accuracy;
+#endif
+        }
+    }
+}
+
---
+++ src/Windows.Devices.Sensors/GPSInternal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Devices.Sensors.Metadata;
+using Windows.Devices.Sensors.ObjectModel;
+
+namespace Windows.Devices.Sensors
+{
+    [SensorDescription("ED4CA589-327A-4FF9-A560-91DA4B48275E")]
+    internal class GPSInternal : Sensor
+    {
+        private static Guid SensorDataKey = new Guid("055C74D8-CA6F-47D6-95C6-1ED3637A0FF4");
+
+        public GPSInternal() : base() { }
+
+        public Location CurrentLocation
+        {
+            get
+            {
+                if (this.DataReport == null && !base.TryUpdateData()){
+                    Location location = new Location();
+                    location.Latitude = 0;
+                    location.Longitude = 0;
+                    location.Accuracy = double.PositiveInfinity;
+                    try
+                    {
+                        base.UpdateData();                        
+                    }
+                    catch (SensorPlatformException err)
+                    {
+                        location.NMEA = "err:" + err.InnerException.Message;
+                    }
+                    return location;
+                }
+                return GetLocation(this.DataReport);
+            }
+        }
+
+        private Location GetLocation(SensorReport report)
+        {
+            if (report == null) { throw new ArgumentNullException("report"); }
+            Location location = new Location();
+            location.Timestamp = report.TimeStamp;
+        /*
+         * https://msdn.microsoft.com/en-us/library/windows/desktop/dd318981(v=vs.85).aspx
+         * SENSOR_DATA_TYPE_GPS_STATUS (PID = 33) VT_I4 1=Valid, 2= not valid
+         * SENSOR_DATA_TYPE_ERROR_RADIUS_METERS (PID = 22) VT_R8
+         * SENSOR_DATA_TYPE_LATITUDE_DEGREES (PID = 2) VT_R8
+         * SENSOR_DATA_TYPE_LONGITUDE_DEGREES (PID = 3) VT_R8
+         * SENSOR_DATA_TYPE_SATELLITES_IN_VIEW (PID = 17) VT_I4
+         * SENSOR_DATA_TYPE_SPEED_KNOTS (PID = 6) VT_R8
+         * SENSOR_DATA_TYPE_NMEA_SENTENCE (PID = 38) 
+         * 
+         */
+            location.NMEA = report.Values[SensorDataKey][25].ToString();
+            try
+            {
+                location.Latitude = (double)report.Values[SensorDataKey][0];
+                location.Longitude = (double)report.Values[SensorDataKey][1];
+                location.Speed = 1.85 * (double)report.Values[SensorDataKey][5];
+                location.Accuracy = (double)report.Values[SensorDataKey][2];
+            }
+            catch (InvalidCastException err)
+            {
+                location.NMEA = "error in cast to double";
+            }
+            try
+            {
+                location.Satelites = (Int32)report.Values[SensorDataKey][13];
+            }
+            catch (InvalidCastException err)
+            {
+                location.NMEA = "error in cast to int";
+            }
+            
+#if DEBUG
+            int cnt = report.Values[SensorDataKey].Count;
+            String[] repValues = new String[cnt];
+            for( int i=0;i<cnt; i++){
+                var value = report.Values[SensorDataKey][i];
+
+                repValues[i] = " " + i + "[" + value.GetType() + "]\t= " + value;
+            }
+            location.debugString = "[" + report.Values[SensorDataKey].Count + "]\n " +
+                String.Join("\n", repValues);
+            location.nativeReportValues = report.Values[SensorDataKey];
+#endif
+            return location;
+        }
+
+    }
+}
